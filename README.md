707fe70ae0d4214a825c9344e558e51f61ba1af6
7th-faily <faily7@outlook.com>
2018-11-17 13:23:52 +0900

    公開

--- README.md
+++ README.md
@@ -1,2 +1,176 @@
-# dbrouter
-DBRouter is single file PHP5.3+ router, create JSON API to direct access DB using PDO.
+# DBRouter
+![version 0.0.1](https://img.shields.io/badge/version-0.0.1-red.svg) ![licence MIT](https://img.shields.io/badge/licence-MIT-blue.svg)
+DBRouterはシングルファイル構成のPHP5.3+用ルーターです。PDOを利用してデータベースに直接アクセスするJSON APIを作ることができます。
+
+# 導入
+PHP5.3以上の環境が必要です。
+1. ルーターを使いたいディレクトリへdbrouter.phpを設置します。
+2. Apacheサーバーの場合、当該ディレクトリの.htaccessに下記を記述します。
+
+```apache
+RewriteEngine on
+RewriteCond %{REQUEST_FILENAME} !-f
+RewriteCond %{REQUEST_FILENAME} !-d
+RewriteRule . index.php [L]
+```
+3. index.phpファイルにルーティング規則を記述していきます。
+
+# 例
+MySQLでユーザー情報を取得する。
+```php
+require_once 'dbrouter.php';
+
+$router = new DBRouter('/api');
+$router->pdo = new PDO('mysql:dbname=db1;host=localhost','account','password');
+
+// Request: /api/user/12
+// Return: [{"id":12;"name":"Paul"}]
+$router->get('/user/[:id]','SELECT * FROM user WHERE id = :id');
+
+exit;
+```
+
+# ルーターの初期化
+
+DBRouterを利用するにはまず、インスタンスを生成します。このとき、パラメータにルーティング基準となるパスを渡すことができます。
+
+### [例] ルートディレクトリを基準とする場合
+```php
+$router = new DBRouter();
+```
+
+### [例] apiディレクトリを基準とする場合
+```php
+$router = new DBRouter('/api');
+```
+
+## PDOの設定
+DBRouterでデータベースアクセス機能を利用する場合、pdoプロパティにPDOインスタンスを設定します。データベースアクセス機能を利用しない場合はこの作業は必要ありません。
+```php
+$router->pdo = new PDO('...');
+```
+PDOインスタンスはDBRouter以外でも利用することが多いため、グローバル変数に入れておくほうが使いやすいかもしれません。
+```php
+$pdo = new PDO('...');
+$router->pdo = $pdo;
+```
+
+# ルーティング
+DBRouterでルーティングを行うには、getもしくはpostメソッドを利用します。getとpostの両方のリクエストを受け付けたい場合は同じパラメータ2行メソッドを書きます。
+
+```php
+public function DBRouter::get(string $route, mixed $query [, bool $end = true])
+```
+```php
+public function DBRouter::post(string $route, mixed $query [, bool $end = true])
+```
+## $routeパラメータ（ルート）
+$routeにはリクエストを受け付けるスラッシュ区切りのパスとアンパサンド区切りのクエリパラメータを指定します。ここでは文字列だけでなく [...] で囲まれた[名前付きパラメータ](#名前付きパラメータ)を受け入れることができます。[名前付きパラメータ](#名前付きパラメータ)については後述します。ここではは変数と捉えてください。
+
+### 例
+```php
+'/user/[:id]/profile'
+```
+$routeには、パスだけでなく受け取るクエリパラメータ（POSTデータ）を指定できます。例えば下記のルートでは、kindパラメータを付けていないリクエストにはマッチしません。必須ではないパラメータを受け入れたい場合は[デフォルト値](##デフォルト値)を参照してください。
+```php
+'/product/search?[:kind]'
+```
+
+## $queryパラメータ（クエリ）
+$queryはSQL文、関数もしくはそれらを組み合わせた配列を指定できます。
+
+## SQL文
+SQL文については[PDO::prepare](http://php.net/manual/ja/pdo.prepare.php)の記述方法に準じます。$routeで指定した[名前付きパラメータ](#名前付きパラメータ)を利用できます。
+$queryにSQL文を指定するか、配列の最後をSQL文にした場合、get/postメソッドはSQL文を実行した結果をjsonデータで出力して処理を終了します。
+
+### 例
+```php
+$router->get('/user/[:id]','SELECT * FROM user WHERE id = :id');
+```
+
+## 関数
+無名関数もしくは可変関数を利用できます。関数はDBRouterインスタンスにpdoプロパティを設定していなくても利用できるので、データベースにこだわらない自由なルーティングが可能です。関数は[名前付きパラメータ](#名前付きパラメータ)を$param配列で受け取ります。
+### [例] 無名関数
+```php
+$router->get('/user/[:id]',function($param){
+	echo $param['id']->value;
+});
+```
+### [例] 可変関数
+```php
+$router->get('/user/[:id]','get_profile');
+
+function get_profile($param) {
+	echo $param['id']->value;
+}
+```
+
+## 配列
+SQL文と関数を組み合わせた配列を$queryに指定できます。例えば以下の例では企業IDとして受け取った文字列から先頭の2文字を削除してからSQL文を実行します。
+```php
+$router->get('/company/[:cid]',array('cid_filter','SELECT * FROM company WHERE cid = :cid'));
+
+function cid_filter($param) {
+	$param['cid']->value = substr($param['cid']->value, 2);
+	return $param;
+}
+```
+
+# 名前付きパラメータ
+ルートを記述する際に用いる [...] で囲まれた部分を**名前付きパラメータ**と呼びます。
+名前の由来はPDOの名前付きパラメータです。
+名前付きパラメータはリクエストから受け取ったパラメータをSQLに受け渡す役割を持ちます。入力値バリデーションとデフォルト値の設定が可能です。セッション値やCookie値を受け取る記法があります。
+
+## 名前付きパラメータの構成
+名前付きパラメータはコロンから始まる**ネーム部**とコロンよりも前に記述する**タイプ部**とイコールから始まる**デフォルト部**から成ります。
+ただし、パスを定義するときはデフォルト値は利用できません。
+```php
+'[type:name=default]'
+```
+
+## ネーム部
+コロンから始まる部分をネーム部と呼びます。名前付きパラメータにおいてこの部分は必須です。
+リクエストから受け入れてPDOのSQL文に渡す名前付きパラメータの名前を定義します。
+
+## タイプ部
+コロンよりも前に記述する部分をタイプ部と呼びます。リクエストから受け入れる入力値のタイプを決めます。
+タイプは入力値をどこから受け取るか、どんな値を受け取るかを表します。
+入力値はクエリに渡される前にあらかじめ定義したバリデーションが適応され、タイプに合わない値が渡されるとルートにマッチしません。
+
+### 定義済みタイプ
+|タイプ |効果 |
+|---|---|
+|str|どんな入力値でも受けとります。タイプ部を省略した場合、このタイプとなります。|
+|int|整数値のみ受け取ります。|
+|session|入力値をリクエストからではなく$_SESSIONから受け取ります。|
+|cookie|入力値をリクエストからではなく$_COOKIEから受け取ります。|
+|server|入力値をリクエストからではなく$_SERVERから受け取ります。|
+|~~files~~ *予約|~~入力値をリクエストからではなく$_FILESから受け取ります。~~|
+|env|入力値をリクエストからではなく$_ENVから受け取ります。|
+|get|入力値をPOSTリクエストの場合もリクエストパラメータから受け取ります。|
+
+### バリデーション
+タイプはvalidメソッドを用いて新しく作ることができます。
+```php
+public function DBRouter::type(string $name, function $valid [, string $parent = 'str'])
+```
+第一引数には新しく定義するタイプ名、第二引数には入力値をとりバリデーション結果を返す関数、第三引数には基となるタイプを指定します。
+次の例は
+
+## デフォルト部
+イコールから始まる部分をデフォルト部と呼びます。
+デフォルト部を省略した場合、その名前付きパラメータは入力値が必須となり、入力値が無いときはルートにマッチしません。
+逆にデフォルト部を記述した場合、入力値が無くてもルートにマッチして、イコールよりも後ろに記述した値がクエリに渡されます。
+また、イコールよりも後ろの値を省略してイコールのみ記述した場合、デフォルト値は空文字となります。
+
+#ライセンス
+
+MIT License
+
+Copyright (c) 2018 7th faily
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
---
+++ dbrouter.php
@@ -0,0 +1,255 @@
+<?php
+/*
+
+MIT License
+
+Copyright (c) 2018 7th faily
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+*/
+
+
+
+class DBRouter {
+	private $request = array();
+	private $valid = array();
+
+	/**
+	 * @var データベースアクセスに利用するPHP Data Objects
+	 * @link http://php.net/manual/ja/book.pdo.php PDOのマニュアル。
+	 */
+	public $pdo = null;
+
+	/**
+	 * 新しくルーターを作る。
+	 * @base_path 基準パスを登録する。指定しなかった場合基準パスはルートパスとなる。
+	 */
+	public function __construct($base_path = '') {
+
+		$url = explode('?',$_SERVER['REQUEST_URI']);
+		$this->request = explode('/',substr($url[0],strlen($base_path)));
+
+		//デフォルトバリデーションを登録
+		$this->valid[''] = array(function($value){return true;},'');
+		$this->valid['str'] = array($this->valid[''][0],'');
+		$this->valid['int'] = array(function($value){return ctype_digit($value);},'');
+		$this->valid['session'] = array($this->valid[''][0],'');
+		$this->valid['cookie'] = array($this->valid[''][0],'');
+		$this->valid['server'] = array($this->valid[''][0],'');
+		$this->valid['files'] = array($this->valid[''][0],'');
+		$this->valid['env'] = array($this->valid[''][0],'');
+		$this->valid['get'] = array($this->valid[''][0],'');
+	}
+
+	/**
+	 * ルーターにgetリクエスト時の動作を登録する。
+	 * @param string $route ルーターに登録するルート条件。
+	 * @param string|function|array $query 条件に一致した場合に実行する処理内容。
+	 * @param bool $end 条件に一致してクエリが終了した後に処理を終了する場合はtrue、終了しない場合はfalseを指定する。初期値はtrue。
+	 * @return bool 条件に一致してクエリが終了したら最後に実行したクエリの戻り値、条件に一致しなかったたらfalseを返す。
+	 */
+	public function get($route,$query,$end = true) {
+
+		//getリクエストでない場合は失敗
+		if(strtolower($_SERVER['REQUEST_METHOD']) !== 'get') return false;
+
+		$params = $this->resolution(INPUT_GET,$route);
+		if($params === false) return false;
+
+		//クエリを実行する
+		return $this->execute($query, $params, $end);
+	}
+
+	/**
+	 * ルーターにPOSTリクエスト時の動作を登録する。
+	 * @param string $route ルーターに登録するルート条件。
+	 * @param string|function|array $query 条件に一致した場合に実行する処理内容。
+	 * @param bool $end 条件に一致してクエリが終了した後に処理を終了する場合はtrue、終了しない場合はfalseを指定する。初期値はtrue。
+	 * @return bool 条件に一致してクエリが終了したら最後に実行したクエリの戻り値、条件に一致しなかったたらfalseを返す。
+	 */
+	public function post($route,$query,$end = true) {
+
+		//getリクエストでない場合は失敗
+		if(strtolower($_SERVER['REQUEST_METHOD']) !== 'post') return false;
+
+		$params = $this->resolution(INPUT_POST,$route);
+		if($params === false) return false;
+
+		//クエリを実行する
+		return $this->execute($query, $params, $end);
+	}
+
+	/**
+	 * タイプを追加する。
+	 * @param string $name 登録するタイプ名。
+	 * @param function $valid 成功したらtrue、失敗したらfalseを返す関数。
+	 * @param string $parent 親とするバリデーション名。初期値は'str'。
+	 * @return bool 成功したらtrue、失敗したらfalseを返す。
+	 */
+	public function type($name, $valid, $parent = 'str') {
+
+		// 登録済みのタイプ名もしくは空文字の場合は失敗。
+		if(array_key_exists($name, $this->valid) || $name === '') return false;
+
+		// 親タイプ名が存在しない場合は失敗
+		if(!array_key_exists($parent, $this->valid)) return false;
+
+		// バリデーションが関数でない場合は失敗
+		if(!is_callable($valid)) return false;
+		$this->valid[$type] = array($valid,$parent);
+	}
+
+	/**
+	 * バリデーションを実行する。
+	 * @param string $type 実行するバリデーション名。
+	 * @param string $value 検査する値。
+	 * @return bool バリデーションに通ったらtrue、通らなかったらfalseを返す。
+	 */
+	public function run_valid($type, $value) {
+
+		// バリデーション名が存在しない場合は失敗
+		if(!array_key_exists($type, $this->valid)) return false;
+
+		// バリデーション名から値を検査
+		while($type != '') {
+			$valids[] = $this->valid[$type][0];
+			$type =  $this->valid[$type][1];
+		}
+		while($valid = array_pop($valids)) {
+			if(!$valid($value)) return false;
+		}
+		return true;
+	}
+	private function base_type($type) {
+		for(;$next = $this->valid[$type][1] !== '';$type = $next);
+		return $type;
+	}
+
+
+	private function resolution($method,$route) {
+		$cond = explode('?',$route);
+		$step = explode('/',$cond[0]);
+		$para = explode('&',$cond[1]);
+		$params = [];
+
+		//そもそもパス数が一致しない場合は失敗
+		if(count($step)!==count($this->request)) return false;
+
+		//各ディレクトリ名が条件に一致するか検査
+		for($i=0;$i<=count($step);$i++){
+			if(preg_match('/^\[(.*):(.+)\]$/',$step[$i],$matches)){
+				if($matches[1]==='') $matches[1] = 'str';
+				if($matches[1]==='session'||$matches[1]==='server'||$matches[1]==='cookie'||$matches[1]==='files'||$matches[1]==='env'||$matches[1]==='get') return false;
+
+				// バリデーション
+				if($this->run_valid($matches[1], $this->request[$i]))
+					$params[$matches[2]] = array('type'=>$matches[1], 'value'=>$this->request[$i]);
+				else return false;
+			}else{
+				if($step[$i]!==$this->request[$i]) return false;
+			}
+		}
+
+		//クエリパラメータの条件に一致するか検査
+		foreach($para as $q) {
+			if($q==='') continue;
+			if(preg_match('/^\[(.*):([_a-zA-Z][_a-zA-Z0-9]*)(=(.*))?\]$/',$q,$matches)){
+				switch(base_type($matches[1])) {
+					case 'session':
+						$value = $_SESSION[$matches[2]];
+						break;
+					case 'server':
+						$value = filter_input(INPUT_SERVER,$matches[2]);
+						break;
+					case 'cookie':
+						$value = filter_input(INPUT_COOKIE,$matches[2]);
+						break;
+					case 'env':
+						$value = filter_input(INPUT_ENV,$matches[2]);
+						break;
+					case 'get':
+						$value = filter_input(INPUT_GET,$matches[2]);
+						break;
+					case '':
+						$matches[1] = 'str';
+					default:
+						$value = filter_input($method,$matches[2]);
+						break;
+				}
+				if($value==null)
+					if($matches[3]) $value = $matches[4];
+					else return false; //必須パラメータ
+
+				// バリデーション
+				if($this->run_valid($matches[1], $value))
+					$params[$matches[2]] = array('type'=>$matches[1], 'value'=>$value);
+				else return false;
+			}else{
+				return false; //記載エラー
+			}
+		}
+
+		return $params;
+	}
+
+	private function execute($queries, $params, $end) {
+		if(!is_array($queries)) $queries = [$queries];
+
+		foreach($queries as $query) {
+			$param = $this->single_execute($query,$params);
+		}
+
+		if($end){
+			if(!is_callable(end($queries)))
+				echo json_encode($param,JSON_UNESCAPED_UNICODE);
+			exit;
+		}
+
+		return $param;
+	}
+	private function single_execute($query,$params) {
+
+		//関数のとき
+		if(is_callable($query)){
+			return $query($params);
+		}
+
+		//DBクエリのとき
+		$stmt = $this->pdo->prepare($query);
+		preg_match_all('/:([a-zA-Z][a-zA-Z0-9]*)/', $query, $matches);
+		foreach($matches[1] as $name) {
+			if(!array_key_exists($name, $params)) throw new Exception($name.' param is not found.');
+			switch(base_type($params[$name]['type'])){
+				case 'int':
+					$stmt->bindparam(':'.$key,$parames[$name]['value'],PDO::PARAM_INT);
+					break;
+				case 'session':
+				case 'server':
+				case 'cookie':
+				case 'get':
+				case 'str':
+					$stmt->bindparam(':'.$key,$params[$name]['value'],PDO::PARAM_STR);
+					break;
+			}
+		}
+		$stmt->execute();
+		return $stmt->fetchAll(PDO::FETCH_ASSOC);
+	}
+}
